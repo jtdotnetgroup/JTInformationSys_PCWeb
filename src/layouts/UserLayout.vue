2eef3e3359598ed4595898380a1c2187ceab081d
xxegoi <250242742@qq.com>
2019-04-26 12:07:32 +0800

    登录页面改完

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -5,11 +5,11 @@
         <div class="header">
           <a href="/">
             <img src="~@/assets/logo.svg" class="logo" alt="logo">
-            <span class="title">Ant Design</span>
+            <span class="title">捷特信息管理系统</span>
           </a>
         </div>
         <div class="desc">
-          Ant Design 是西湖区最具影响力的 Web 设计规范
+          高效、精准、快速
         </div>
       </div>
 
@@ -22,7 +22,7 @@
           <a href="_self">条款</a>
         </div>
         <div class="copyright">
-          Copyright &copy; 2018 白鹭学园技术组出品
+          Copyright &copy; 2019 捷特软件
         </div>
       </div>
     </div>
