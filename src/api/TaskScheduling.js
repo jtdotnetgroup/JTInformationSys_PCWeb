bfd126ec1de1a16a65559e99de4e1111fc5a24ee
xxegoi <250242742@qq.com>
2019-05-15 09:56:49 +0800

    Revert "OK"
    This reverts commit cef272b3cf3c1d756254b35dd7b8b10f17242b44.

--- src/api/TaskScheduling.js
+++ src/api/TaskScheduling.js
@@ -1,7 +1,6 @@
 import { axios } from '@/utils/request'
 
 export function GetTaskSchedulData (params) {
-  // 查所有任务单接口
   return axios({
     url: '/api/services/app/TaskScheduling/GetAll',
     method: 'get',
@@ -10,28 +9,17 @@ export function GetTaskSchedulData (params) {
 }
 
 export function GetAllDailyList (params) {
-  // 查所有日计划单接口
   return axios({
-    url: '/api/services/app/ICMODispBill/GetAll',
+    url: 'api/services/app/ICMODispBill/GetAll',
     method: 'get',
     params: params
   })
 }
 
 export function SaveDailyList (data) {
-  // 保存日计划单接口
   return axios({
     url: '/api/services/app/ICMODaily/Create',
     method: 'post',
     data: data
-  })
-}
-
-export function GetAllDailyByFMOInterID (params) {
-  // 根据任务单ID获取所有日计划单排产数据
-  return axios({
-    url: '/api/services/app/ICMODaily/GetDialyQtyListByFMOInterID',
-    method: 'get',
-    params: params
   })
 }
