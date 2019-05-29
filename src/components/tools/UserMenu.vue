5413f72fb9113929699b1b579df539a161c57442
CYX15916011519 <50003846+CYX15916011519@users.noreply.github.com>
2019-05-29 10:31:37 +0800

    账户设置、全屏
    账户设置、全屏

--- src/components/tools/UserMenu.vue
+++ src/components/tools/UserMenu.vue
@@ -6,12 +6,20 @@
           <a-icon type="question-circle-o"></a-icon>
         </span>
       </a>
+      <span class="action" @click="handleFullScreen">
+        <a-icon v-show="Isfullscreen" type="fullscreen-exit"/>
+        <a-icon v-show="!Isfullscreen" type="fullscreen"/>
+      </span>
       <notice-icon class="action"/>
       <a-dropdown>
         <span class="action ant-dropdown-link user-dropdown-menu">
           <!-- <a-avatar class="avatar" size="small" :src="avatar()"/>
-          <span>{{ nickname() }}</span> -->
-          <a-avatar class="avatar" size="small" src="~/public/ZRL.jpg"/>
+          <span>{{ nickname() }}</span>-->
+          <a-avatar
+            class="avatar"
+            size="small"
+            src="http://hbimg.b0.upaiyun.com/ea52c1c9e5df4039ce5275095a0649921ad083244d20a-Lf82xv_fw658"
+          />
           <span>1001</span>
         </span>
         <a-menu slot="overlay" class="user-dropdown-menu-wrapper">
@@ -49,6 +57,11 @@ import NoticeIcon from '@/components/NoticeIcon'
 import { mapActions, mapGetters } from 'vuex'
 
 export default {
+  data() {
+    return {
+      Isfullscreen: false
+    }
+  },
   name: 'UserMenu',
   components: {
     NoticeIcon
@@ -56,25 +69,55 @@ export default {
   methods: {
     ...mapActions(['Logout']),
     ...mapGetters(['nickname', 'avatar']),
-    handleLogout () {
+    handleLogout() {
       const that = this
 
       this.$confirm({
         title: '提示',
         content: '真的要注销登录吗 ?',
-        onOk () {
-          return that.Logout({}).then(() => {
-            window.location.reload()
-          }).catch(err => {
-            that.$message.error({
-              title: '错误',
-              description: err.message
+        onOk() {
+          return that
+            .Logout({})
+            .then(() => {
+              window.location.reload()
             })
-          })
+            .catch(err => {
+              that.$message.error({
+                title: '错误',
+                description: err.message
+              })
+            })
         },
-        onCancel () {
-        }
+        onCancel() {}
       })
+    },
+    // 全屏事件
+    handleFullScreen() {
+      let element = document.documentElement
+      if (this.fullscreen) {
+        if (document.exitFullscreen) {
+          document.exitFullscreen()
+        } else if (document.webkitCancelFullScreen) {
+          document.webkitCancelFullScreen()
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen()
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen()
+        }
+      } else {
+        if (element.requestFullscreen) {
+          element.requestFullscreen()
+        } else if (element.webkitRequestFullScreen) {
+          element.webkitRequestFullScreen()
+        } else if (element.mozRequestFullScreen) {
+          element.mozRequestFullScreen()
+        } else if (element.msRequestFullscreen) {
+          // IE11
+          element.msRequestFullscreen()
+        }
+      }
+      this.fullscreen = !this.fullscreen
+      this.Isfullscreen = this.fullscreen
     }
   }
 }
--- src/config/router.config.js
+++ src/config/router.config.js
@@ -17,13 +17,13 @@ export const asyncRouterMap = [
         redirect: '/dashboard/workplace',
         component: RouteView,
         hideChildrenInMenu: true,
-        meta: { title: '工作台', keepAlive: true, icon: 'home' },
+        meta: { title: '工作中心', keepAlive: true, icon: 'home' },
         children: [
           {
             path: '/dashboard/workplace',
             name: 'Workplace',
             component: () => import('@/views/dashboard/Workplace'),
-            meta: { title: '工作台', keepAlive: true }
+            meta: { title: '工作中心', keepAlive: true }
           }
         ]
       },
@@ -120,6 +120,62 @@ export const asyncRouterMap = [
             component: () => import('@/views/Report/Setting/index')
           }
         ]
+      },
+      // account
+      {
+        path: '/account',
+        component: RouteView,
+        redirect: '/account/center',
+        name: 'account',
+        meta: { title: '个人页', icon: 'user', keepAlive: true, permission: [ 'user' ] },
+        children: [
+          {
+            path: '/account/center',
+            name: 'center',
+            component: () => import('@/views/account/center/Index'),
+            meta: { title: '个人中心', keepAlive: true, permission: [ 'user' ] }
+          },
+          {
+            path: '/account/settings',
+            name: 'settings',
+            component: () => import('@/views/account/settings/Index'),
+            meta: { title: '个人设置', hideHeader: true, permission: [ 'user' ] },
+            redirect: '/account/settings/base',
+            hideChildrenInMenu: true,
+            children: [
+              {
+                path: '/account/settings/base',
+                name: 'BaseSettings',
+                component: () => import('@/views/account/settings/BaseSetting'),
+                meta: { title: '基本设置', hidden: true, permission: [ 'user' ] }
+              },
+              {
+                path: '/account/settings/security',
+                name: 'SecuritySettings',
+                component: () => import('@/views/account/settings/Security'),
+                meta: { title: '安全设置', hidden: true, keepAlive: true, permission: [ 'user' ] }
+              },
+              {
+                path: '/account/settings/custom',
+                name: 'CustomSettings',
+                component: () => import('@/views/account/settings/Custom'),
+                meta: { title: '个性化设置', hidden: true, keepAlive: true, permission: [ 'user' ] }
+              },
+              {
+                path: '/account/settings/binding',
+                name: 'BindingSettings',
+                component: () => import('@/views/account/settings/Binding'),
+                meta: { title: '账户绑定', hidden: true, keepAlive: true, permission: [ 'user' ] }
+              },
+              {
+                path: '/account/settings/notification',
+                name: 'NotificationSettings',
+                component: () => import('@/views/account/settings/Notification'),
+                meta: { title: '新消息通知', hidden: true, keepAlive: true, permission: [ 'user' ] }
+              }
+            ]
+          }
+        ]
       }
     ]
   },
@@ -156,11 +212,20 @@ export const constantRouterMap = [
       }
     ]
   },
-
+  {
+    path: '/403',
+    name: '403',
+    component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/403')
+  },
   {
     path: '/404',
     name: '404',
     component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/404')
+  },
+  {
+    path: '/500',
+    name: '500',
+    component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/500')
   }
 
 ]
